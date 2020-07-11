7060e9fb23496caa2e7a3b41af8f3ba0e14d9eac
andusha <57403896+andusha@users.noreply.github.com>
2020-07-11 19:50:54 +0700

    Update bot-discord.py

--- bot-discord.py
+++ bot-discord.py
@@ -1,11 +1,11 @@
 import discord
 from discord.ext import commands
-
-TOKEN = 'NzMxMDk0MzA3NjAzMzQ5NTc1.XwhWrA.6oIAa1QPw5pmve7qBxOhh66hJ0M'
+import os
 
 bot = commands.Bot(command_prefix='`')
 @bot.command(pass_context=True)
 async def finger(ctx, arg):  # создаем асинхронную фунцию бота
     await ctx.send(arg)
-    
-bot.run(TOKEN)
+
+
+bot.run(os.environ.get('BOT_TOKEN'))
